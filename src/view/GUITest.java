1c52ff87107bbe2f0b3a7c884fbb0c903ce95078
千里冰封 <zhangyinsen@126.com>
2016-01-31 21:07:56 +0800

    增加两个拿来玩的东西

---
+++ src/view/GUITest.java
@@ -0,0 +1,17 @@
+package view;
+
+import javax.swing.*;
+
+/**
+ * 用图形化界面生成的类
+ * Created by asus1 on 2016/1/31.
+ */
+public class GUITest {
+	private JPanel panel;
+	private JTextField textField1;
+	private JTextArea textArea1;
+
+	private void createUIComponents() {
+		// TODO: place custom component creation code here
+	}
+}
