5296ccf67e6dcbeadf13f5424fc813ffbf624ad0
千里冰封 <zhangyinsen@126.com>
2016-02-02 23:10:47 +0800

    终于可以显示地图了。。。

--- src/funcs/using/FuncMap.java
+++ src/funcs/using/FuncMap.java
@@ -21,19 +21,23 @@ public class FuncMap extends FuncSrc{
 	public void DoFunc(String cmd) {
 //		.
 		JFrame frame = new JFrame("地图");
-		JPanel panel = new JPanel() {
-			@Override
-			protected void paintComponent(Graphics g) {
-//				ImageIcon icon = new ImageIcon("." + File.separator + "drawable" + File.separator + "map.png");
-				ImageIcon icon = new ImageIcon("C:\\Users\\asus1\\Desktop\\城堡游戏\\drawable\\map.png");
-				Image img = icon.getImage();
-				g.drawImage(img, 0, 0, icon.getIconWidth(),
-						icon.getIconHeight(), icon.getImageObserver());
-				frame.setSize(icon.getIconWidth(), icon.getIconHeight());
-			}
-		};
-		frame.add(panel);
-	//	frame.pack();
+		JPanel panel = new JPanel();
+		JLabel label = new JLabel();
+		Image image = Toolkit.getDefaultToolkit().getImage(
+				"." + File.separator + "src" + File.separator + "drawable" + File.separator + "map.png"
+		);
+		ImageIcon icon = new ImageIcon(image);
+//		ImageIcon icon = new ImageIcon(".\\233.png");
+//		Image img = icon.getImage();
+//		g.drawImage(img, 0, 0,
+//				icon.getIconWidth(),
+//				icon.getIconHeight(),
+//				icon.getImageObserver()
+//		);
+		label.setIcon(icon);
+		frame.setSize(icon.getIconWidth(), icon.getIconHeight());
+		panel.add(label);
+		frame.setContentPane(panel);
 		frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
 		frame.setVisible(true);
 	}
