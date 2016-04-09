91a2288ae533b5dd04a20220341dff44c193787e
ICE1000 <951394653@qq.com>
2016-04-09 19:42:50 +0800

    Convert files into Kotlin

--- src/util/NameGenerator.kt
+++ src/util/NameGenerator.kt
@@ -12,8 +12,8 @@ class NameGenerator {
     }
 }
 
-fun generate(): String? {
-    val names = arrayOf("金木研", "赤羽业", "泉新一", "千里冰封", "奶茶", "3A", "无", "SpiderKing", "Direction.D.", "Timothy")
-    val i = Math.random()
-    return names[(names.size * i).toInt()]
-}
+//fun generate(): String? {
+//    val names = arrayOf("金木研", "赤羽业", "泉新一", "千里冰封", "奶茶", "3A", "无", "SpiderKing", "Direction.D.", "Timothy")
+//    val i = Math.random()
+//    return names[(names.size * i).toInt()]
+//}
